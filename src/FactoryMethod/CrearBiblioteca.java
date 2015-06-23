22ec92cdbd8fc5b73413e8ac214b9860e8bf7d4e
EST1659109 <EST1659109@192.168.56.1>
2015-06-23 11:57:42 -0600

    Modificacion de la interfaz Biblioteca y la la clase "fabrica (CrearBiblioteca) "

--- src/FactoryMethod/CrearBiblioteca.java
+++ src/FactoryMethod/CrearBiblioteca.java
@@ -4,19 +4,18 @@
  * and open the template in the editor.
  */
 package FactoryMethod;
-import patrones.conexion;
 
 /**
  *
  * @author EST1659109
  */
-public interface Biblioteca {
-    public conexion conectar=null;
-    public Libros libro=null;
-    public CrearBiblioteca fabrica=null;
-    
-    //metodo de fabricacion 
-    public Libros EnlazarAutorLibro();
-   
+public class CrearBiblioteca implements Biblioteca{
+    Libros libro;
+
+    //@Override
+    public Libros EnlazarAutorLibro(Libros Nlibro) {
+        return libro;
+    }
+
     
 }
--- src/FactoryMethod/Libros.java
+++ src/FactoryMethod/Libros.java
@@ -4,19 +4,11 @@
  * and open the template in the editor.
  */
 package FactoryMethod;
-import patrones.conexion;
 
 /**
  *
  * @author EST1659109
  */
-public interface Biblioteca {
-    public conexion conectar=null;
-    public Libros libro=null;
-    public CrearBiblioteca fabrica=null;
-    
-    //metodo de fabricacion 
-    public Libros EnlazarAutorLibro();
-   
-    
+public interface Libros {
+ 
 }
